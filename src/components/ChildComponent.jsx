82c7d00c42eb1b0de4d8b4510ccb16b7ecd362c7
Marcin Mazurek <marcin@mazurek.pro>
2016-09-05 18:00:13 +0100

    Clean up

--- src/components/ChildComponent.jsx
+++ src/components/ChildComponent.jsx
@@ -3,19 +3,19 @@ import visualiseRender from 'react-render-visualizer-decorator';
 
 @visualiseRender
 export default class ChildComponent extends Component {
-  generateNumber () {
+  generateNumber() {
     this.setState({
       number: Math.ceil(Math.random() * 1000000000)
-    })
+    });
   }
 
-  disableSelect (e) {
+  disableSelect(e) {
     e.preventDefault();
   }
 
-  render () {
-    return (<div className="childComponent" onClick={() => this.forceUpdate()} onMouseDown={e => this.disableSelect(e)}>
-      Click to generate a random number. <strong>{Math.ceil(Math.random() * 1000000000)}</strong>
+  render() {
+    return (<div className="childComponent" onClick={() => this.generateNumber()} onMouseDown={e => this.disableSelect(e)}>
+      Click to generate a random number. <strong>{this.state.number}</strong>
     </div>);
   }
 }
